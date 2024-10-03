9303cc9e9ad5f7652c5ee0b2d2ed6b2ee6fdfb50
nati2331 <natnael.mk2@gmail.com>
2024-10-03 23:44:52 +0000

    updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Pascals Triangle"""
+
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -5,8 +8,12 @@ def pascal_triangle(n):
     for row_number in range(1, n):
         row = [1]
         for j in range(1, row_number):
-            element = triangle[row_number - 1][j - 1] + triangle[row_number - 1][j]
+            prev_row = triangle[row_number - 1]
+            left = prev_row[j - 1]
+            right = prev_row[j]
+            element = left + right
             row.append(element)
         row.append(1)
         triangle.append(row)
+
     return triangle
