49dfe6ba90c4e85cda73a092713a72e2664e659e
nati2331 <natnael.mk2@gmail.com>
2024-10-03 23:37:20 +0000

    updates

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Pascals Triangle"""
+"""Triangle Triangle"""
+
 
 def pascal_triangle(n):
+    """Triangle"""
     if n <= 0:
         return []
-    triangle = [[1]]
+    pas = [[1]]
     for row_number in range(1, n):
         row = [1]
         for j in range(1, row_number):
-            element = triangle[row_number - 1][j - 1] + triangle[row_number - 1][j]
+            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
             row.append(element)
         row.append(1)
-        triangle.append(row)
-    return triangle
+        pas.append(row)
 
-if __name__ == "__main__":
-    n = int(input("Enter the number of rows for Pascal's triangle: "))
-    print(pascal_triangle(n))
+    return pas
