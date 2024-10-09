9af1f93f69112ba68aa5a5a25c42b127b15291a5
nati2331 <natnael.mk2@gmail.com>
2024-10-09 11:29:32 +0000

    updates

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,66 +2,41 @@
 """Determines if all boxes can be unlocked."""
 
 
-def find_next_open_box(opened_boxes):
-    """Identifies the next opened box.
-    
-    Args:
-        opened_boxes (dict): Dictionary of boxes that have been opened.
-        
-    Returns:
-        list: Keys found in the opened box.
-    """
-    for index, box in opened_boxes.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
-    return None
-
-
 def canUnlockAll(boxes):
     """Evaluates if all boxes can be accessed.
-    
+
     Args:
         boxes (list): List containing all boxes with their keys.
-        
+
     Returns:
         bool: True if all boxes can be accessed, otherwise False.
     """
-    if len(boxes) <= 1 or boxes == [[]]:
-        return True
-
-    aux = {}
-    while True:
-        if len(aux) == 0:
-            aux[0] = {
-                'status': 'opened',
-                'keys': boxes[0],
-            }
-        keys = find_next_open_box(aux)
-        if keys:
-            for key in keys:
-                try:
-                    if aux.get(key) and aux.get(key).get('status') == 'opened/checked':
-                        continue
-                    aux[key] = {
-                        'status': 'opened',
-                        'keys': boxes[key]
-                    }
-                except (KeyError, IndexError):
-                    continue
-        elif 'opened' in [box.get('status') for box in aux.values()]:
-            continue
-        elif len(aux) == len(boxes):
-            break
-        else:
-            return False
-
-    return len(aux) == len(boxes)
+    n = len(boxes)
+    
+    opened = set([0])
+    keys = set(boxes[0])
+    
+    while keys:
+        new_keys = set()
+        for key in keys:
+            if key < n and key not in opened:
+                opened.add(key)
+                new_keys.update(boxes[key])
+        keys = new_keys
+        
+    return len(opened) == n
 
 
 def main():
     """Main execution function."""
-    canUnlockAll([[]])
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
 
 
 if __name__ == '__main__':
